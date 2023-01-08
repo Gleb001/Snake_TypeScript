2f10cc55c3d30ee31dda6378e4c4a1b7f5f13b14
Gleb Lichagin <89206789+Gleb001@users.noreply.github.com>
2023-01-08 11:45:32 +0500

    Delete dist directory

--- dist/utility/work_with_html.js
+++
@@ -1,9 +0,0 @@
-function createElementHTML({ tag_name = "div", attributes = {}, inner_value = "" }) {
-    let html_element = document.createElement(tag_name);
-    html_element.innerHTML = inner_value;
-    for (let name_attr in attributes) {
-        html_element.setAttribute(name_attr, attributes[name_attr]);
-    }
-    return html_element;
-}
-export default createElementHTML;
